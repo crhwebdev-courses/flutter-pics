f544610630f922f5f509418678c1b1d6f3da7238
Carl Hain <crhain@gmail.com>
2019-03-17 22:46:02 -0500

    Add new function handler for floating button in app.dart that retrieves images from an api

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,7 @@
-// Import flutter helper libarary
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
+import 'models/image_model.dart';
 
 // Create a stateful widget that gets rerendered
 class App extends StatefulWidget {
@@ -13,21 +15,28 @@ class App extends StatefulWidget {
 // Class extends State class, which is a generic that takes the type of class it is rendered by
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
 
   // Must define a 'build' method that returns
   // the widgets that *this* widget will show
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('${counter}'),
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            //update state
-            setState(() {
-              counter++;
-            });
-          },
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text("Let's See some images"),
