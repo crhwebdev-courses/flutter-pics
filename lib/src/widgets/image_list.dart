08648d91be1a9f998fc17df3cde900a365bdab96
Carl Hain <crhain@gmail.com>
2019-03-18 10:58:03 -0500

    Add some styling to ImageList for each image: margin, padding, and a border

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -10,7 +10,14 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Text(images[index].url);
+        return Container(
+          margin: EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+          child: Image.network(images[index].url),
+        );
       },
     );
   }
